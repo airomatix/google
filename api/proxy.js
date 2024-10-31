c938c30b80afc981f32f1c2e22369b44042cb64d
airomatix <101486560+airomatix@users.noreply.github.com>
2024-10-31 09:30:38 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.vexmovies.biz.id/";//your website url
+  let target = "https://www.yahoo.com";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
